bec7df24feea5ef7d6e89765933edce65ede5bd5
Gal <gala0@post.bgu.ac.il>
2022-11-18 15:22:37 +0200

    one error

--- include/JoinPolicy.h
+++ include/JoinPolicy.h
@@ -1,12 +1,17 @@
 #pragma once
-#include "../include/Party.h"
 #include <string>
+#include "Party.h"
+#include "Simulation.h"
 using std::string;
+
+class Party;
+class Simulation;
 
 class JoinPolicy {
 
     public:
     virtual void join(Party& party,Simulation& sim) = 0;
+    virtual ~JoinPolicy();
     virtual string getJoinType() = 0;
     virtual JoinPolicy* clone () = 0;
 };
@@ -15,6 +20,7 @@ class MandatesJoinPolicy : public JoinPolicy
 {
     public:
     MandatesJoinPolicy();
+    virtual ~MandatesJoinPolicy();
     virtual JoinPolicy* clone() override;
     virtual void join(Party& party,Simulation& sim) override; 
     virtual string getJoinType() override;
@@ -24,6 +30,7 @@ class LastOfferJoinPolicy : public JoinPolicy
 {
     public:
     LastOfferJoinPolicy();
+    virtual ~LastOfferJoinPolicy();
     virtual JoinPolicy* clone() override;
     virtual void join(Party& party,Simulation& sim) override;
     virtual string getJoinType() override;
--- include/SelectionPolicy.h
+++ include/SelectionPolicy.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <vector>
 #include "Simulation.h"
-#include "Coalition.h"
+
 
 class SelectionPolicy 
 {
     public:
     virtual void Select(Simulation& simulation,int partyId,vector<int>& partyIds,int agentId) = 0; 
+    virtual ~SelectionPolicy();
     //party ids after filter partys that someone from coalition already invited
     virtual string getSelectionType() = 0;
     virtual SelectionPolicy* clone () = 0;
@@ -16,6 +17,7 @@ class MandatesSelectionPolicy: public SelectionPolicy
 {
     public:
     void Select(Simulation& simulation,int partyId,vector<int>& partyIds,int agentId);
+    virtual ~MandatesSelectionPolicy() override;
     virtual string getSelectionType() override;
     virtual SelectionPolicy* clone() override;
 };
@@ -24,6 +26,7 @@ class EdgeWeightSelectionPolicy: public SelectionPolicy
 { 
     public:
     void Select(Simulation& simulation,int partyId,vector<int>& partyIds,int agentId);
+    virtual ~EdgeWeightSelectionPolicy() override;
     virtual string getSelectionType() override;
     virtual SelectionPolicy* clone() override;
 };
